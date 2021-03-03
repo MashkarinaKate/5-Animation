f9ccaaf10f6d9a42b86dbf13f9b3df2685286351
MashkarinaKate <63970638+MashkarinaKate@users.noreply.github.com>
2021-03-03 23:11:00 +0300

    т

---
+++ Практика 5/Практика 5/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Практика_5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Стереть = new System.Windows.Forms.Button();
+            this.Нарисовать = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Стереть
+            // 
+            this.Стереть.Location = new System.Drawing.Point(315, 0);
+            this.Стереть.Name = "Стереть";
+            this.Стереть.Size = new System.Drawing.Size(88, 35);
+            this.Стереть.TabIndex = 1;
+            this.Стереть.Text = "Стереть";
+            this.Стереть.UseVisualStyleBackColor = true;
+            this.Стереть.Click += new System.EventHandler(this.Стереть_Click);
+            // 
+            // Нарисовать
+            // 
+            this.Нарисовать.Location = new System.Drawing.Point(218, 0);
+            this.Нарисовать.Name = "Нарисовать";
+            this.Нарисовать.Size = new System.Drawing.Size(103, 35);
+            this.Нарисовать.TabIndex = 0;
+            this.Нарисовать.Text = "Нарисовать";
+            this.Нарисовать.UseVisualStyleBackColor = true;
+            this.Нарисовать.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(130, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Оживить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Стереть);
+            this.Controls.Add(this.Нарисовать);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Стереть;
+        private System.Windows.Forms.Button Нарисовать;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Практика 5/Практика 5/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_5
+{
+    public partial class Form1 : Form
+    {
+        bool Draw;
+        int direct;
+        int x, y, r;
+        public Form1()
+        {
+            InitializeComponent();
+            Draw = false;
+            direct = 1;
+            x = 0;
+            y = 0;
+            r = 20;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (Draw)
+            {
+                e.Graphics.FillEllipse(new SolidBrush(Color.Black), x, y, r, r);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Draw = true;
+            this.Invalidate();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            switch (direct)
+            {
+                case 1:
+                    x += 5;
+                    y += 5;
+                    if (x >= this.ClientSize.Width - 20 || y >= this.ClientSize.Height - 20) direct = 2;
+                    break;
+                case 2:
+                    y -= 5;
+                    if (y == 0) direct = 1;
+                    if(y == 0 && x >= this.ClientSize.Width - 20) direct = 3;
+                    break;
+                case 3:
+                    x -= 5;
+                    y += 5;
+                    if (x == 0 || y >= this.ClientSize.Height - 20) direct = 4;
+                    break;
+                case 4:
+                    y -= 5;
+                    if (y == 0) direct = 3;
+                    if(y == 0 && x == 0) direct = 1;
+                    break;
+            }
+
+            this.Invalidate();
+        }
+
+        private void Стереть_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            x = 0; y = 0;
+            Draw = false;
+            direct = 1;
+            this.Invalidate();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
